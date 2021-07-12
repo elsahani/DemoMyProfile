d66435eab13a7d35f0bee6b6c7758cbad091f7a2
20023841 <20023841@myrp.edu.sg>
2021-07-12 10:25:30 +0800

    Completed

--- app/src/main/java/sg/edu/rp/c346/id20023841/demomyprofile/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20023841/demomyprofile/MainActivity.java
@@ -2,13 +2,49 @@ package sg.edu.rp.c346.id20023841.demomyprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etname, etGPA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etname = findViewById(R.id.editTextName);
+        etGPA = findViewById(R.id.editTextGPA);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String gpa = etGPA.getText().toString();
+        float GPA = 0f;
+
+        if(gpa.length() > 0){
+            GPA = Float.parseFloat(gpa);
+        }
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor prefeditor = prefs.edit();
+        prefeditor.putString("name",etname.getText().toString());
+        prefeditor.putFloat("gpa", GPA);
+        prefeditor.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        String name = prefs.getString("name", "");
+        float gpa = prefs.getFloat("gpa", 0f);
+
+        etname.setText(name);
+        etGPA.setText(gpa + "");
     }
 }
